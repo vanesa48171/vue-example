7125ecbecb82c3747180e82b5be70a1f83fd481a
vanesa <vanesa48171@gmail.com>
2019-10-11 19:10:21 -0300

    version 1

---
+++ src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <NavBarPrueba />
+    <input type=text v-model='actual.name'/>
+    <input type=text v-model='actual.lastname'/>
+    <button @click="uploadData()" >Cargar</button>
+    <NavBar />
+    <br/>
+
+    <div v-for="(item,i) in list" :key="i"> 
+      Name: {{item.name}}
+      <br/>
+      Lastname: {{item.lastname}}
+    </div>
+    <NavBar />
+  </div>
+</template>
+
+<script>
+import NavBarPrueba from './components/NavBar'
+import HelloWorld from './components/HelloWorld'
+
+export default {
+  name: "App",
+  components: {
+    NavBarPrueba,
+    
+  },
+  data(){return{
+    list: [],
+    actual: {
+      name: '',
+      lastname: ''
+    }
+  }},
+  methods:{
+    uploadData(){
+      this.list.push(this.actual)
+      this.actual = {
+        name: '',
+        lastname: ''
+      }
+    }
+  }
+};
+</script>
+
+<style>
+
+</style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1> Bienvenide </h1>
+</template>
+
+<script>
+export default {
+  name: 'navbar-componente-x'
+}
+</script>
